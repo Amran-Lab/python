19e86b53ebe96e94d817df3bf16a9027893b6e91
Amran-Lab <amran3210@gmail.com>
2020-02-26 09:13:14 +0000

    family

--- family_tree.py
+++ family_tree.py
@@ -2,17 +2,9 @@
 from family import family_class
 
 tree = {}
-def writ():
-    with open('records.txt', 'w') as f:
-        for key, value in tree.items():
-            f.write('%s:%s' % (key, value))
 
 
-def read():
-    with open('records.txt', 'r') as f:
-        string=f.read()
-        string = string.split(',')
-        print(string)
+
 
 
 read()
